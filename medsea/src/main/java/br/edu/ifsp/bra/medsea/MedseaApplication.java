ed72f64213a442bfda82c7206a1a261fe343be1f
unknown <gabriellybabi10@gmail.com>
2024-10-22 15:51:04 -0300

    Criação do projeto

---
+++ medsea/src/main/java/br/edu/ifsp/bra/medsea/MedseaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.medsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedseaApplication.class, args);
+	}
+
+}
---
+++ medsea/src/test/java/br/edu/ifsp/bra/medsea/MedseaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.medsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
